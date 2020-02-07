6c19f5a54f57b933c69825234c3728c74afae41f
dimstudi0 <dnldimtiri@gmail.com>
2020-02-07 12:50:45 +0100

    Changed the main.py for the online classification

--- main.py
+++ main.py
@@ -1,43 +1,22 @@
 import json
-import os
-import re
 import select
 import socket
+import re
 import threading
-from datetime import datetime
-from datetime import timedelta
-import numpy as np
+import torch
+import joblib
 import pandas as pd
 from pandas.io.json import json_normalize
-from sklearn import preprocessing
-import tensorflow as tf
-from tensorflow import keras
-import plotly.graph_objects as go
-import matplotlib.pyplot as plt
-from pylab import text
+import model_training_pytorch
 
-# tcp-server setting
 bind_ip = '127.0.0.1'
 port_list = [20001]  # could work with 2 ports
-start_script = datetime.now()
 servers = []
+targets = ['classRelease', 'classDepth', 'classRate', 'armsLocked', 'bodyWeight']
+to_exclude = ['Ankle', 'Hip']  # variables to exclude Kinect specific
 request_count = 0
+trained_model = 'models/lstm.pt'
 complete_compression = 0
-
-# general dataframes
-df_kinect = pd.DataFrame()
-df_myo = pd.DataFrame()
-df_all = pd.DataFrame()
-
-# processing settings
-to_exclude = ['Ankle', 'Hip', 'Hand']  # variables to exclude Kinect specific
-targets = ['classRate', 'classDepth', 'classRelease', 'armsLocked', 'bodyWeight']
-
-rock = 0
-fast = 0
-slow = 0
-
-performance = [1]
 
 
 # load the json parsed data
@@ -66,6 +45,7 @@ def json_to_df(data):
     return df
 
 
+
 def start_tcp_server(ip, port):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((ip, port))
@@ -76,7 +56,7 @@ def start_tcp_server(ip, port):
 
 def handle_client_connection(client_socket, port):
     request = client_socket.recv(10000000)
-    # print(request)
+    print(request)
     json_string = json.loads(request, encoding='ascii')
     for jst in json_string:
         if jst is not None:
@@ -87,95 +67,47 @@ def handle_client_connection(client_socket, port):
             elif jst["ApplicationName"] == "Myo":
                 global df_myo
                 df_myo = json_to_df(jst)
-    process_data()
-    client_socket.send("hello back".encode())
-    client_socket.close()
+    return_dict = process_data()
 
+    # return_dict = {}
+    # for t in targets:
+    #     # uncomment this in case of 3 classes
+    #     #if t == 'classDepth' or t == 'classRate':
+    #     #    return_dict[t] = random.randrange(3)
+    #     #else:
+    #     return_dict[t] =  random.randrange(2)
+
+    client_socket.send(str(return_dict).encode())
+    client_socket.close()
 
 def process_data():
     global df_myo
     global df_kinect
     global df_all
     global complete_compression
-    global slow
-    global fast
-    global rock
-    global performance
+    result = {}
     if (not df_myo.empty) and (not df_kinect.empty) and ("Kinect.ShoulderLeftY" in df_kinect):
-
         complete_compression = complete_compression + 1
-
-        if len(performance) > 10:
-            performance = performance[1:]
-        if df_kinect["Kinect.ShoulderLeftY"].index[-1] > timedelta(seconds=0.7):
-            slow = slow + 1
-            feedback = "Too slow"
-            performance.append(0)
-        elif df_kinect["Kinect.ShoulderLeftY"].index[-1] < timedelta(seconds=0.4):
-            fast = fast + 1
-            feedback = "Too fast "
-            performance.append(0)
-        else:
-            rock = rock + 1
-            feedback = "You rock "
-            performance.append(1)
-
-        # incorrect = str("incorrect {:0.0f}".format((1 - np.mean(performance)) * 100) + '%')
-        # correct = str("correct {:0.0f}".format(np.mean(performance) * 100)+'%')
-
-        cc_duration = df_kinect["Kinect.ShoulderLeftY"].index[-1].total_seconds()
-        bpm = 60.0 / cc_duration
-        plot_gauge(bpm, "gauge.png")
         df_all = pd.concat([df_kinect, df_myo], ignore_index=False, sort=False).sort_index()
-        if not df_all.empty:
-            tensor = tensor_transform(df_all, 150, 8)
-            print("tensor transformation "+str(np.shape(tensor)))
-            for t in targets:
-                model_loaded = load_model(t, compile=False)
-                test_predictions = model_loaded.predict(tensor,steps=1)
-                print(('For target'+t+' predictions are'+test_predictions))
-        df_kinect = pd.DataFrame()
-        df_myo = pd.DataFrame()
+        result = online_classification("models/lstm",df_all)
+    return result
 
+def online_classification(path_to_model, input_sample):
+    scaler = joblib.load(f"{path_to_model}_scaler.pkl")
 
-def plot_gauge(bpm, filename):
-    if 50 < bpm < 170:
-        fig = go.Figure(go.Indicator(
-            domain={'x': [0, 1], 'y': [0, 1]},
-            value=bpm,
-            mode="gauge+number+delta",
-            title={'text': "CPR compressionRate"},
-            gauge={'axis': {'range': [50, 170]},
-                   'bar': {'color': "gray"},
-                   'steps': [
-                       {'range': [50, 100], 'color': "white"},
-                       {'range': [100, 120], 'color': "green"},
-                       {'range': [120, 170], 'color': "white"},
-                   ],
-                   'threshold': {'line': {'color': "red", 'width': 4}, 'thickness': 0.75, 'value': bpm}}))
-        fig.write_image(filename)
+    model = model_training_pytorch.MyLSTM()
+    loaded = torch.load(f'{path_to_model}.pt')
+    model = model.load_state_dict(loaded['state_dict'])
+    model.eval()
 
+    scaled_data = scaler.transform(input_sample)
+    prediction = model(scaled_data)
 
-def tensor_transform(df_all, res_rate, bin_size):
-    if not df_all.empty:
-        batch = df_all.resample(str(res_rate) + 'ms').first()[:bin_size].fillna(method='ffill').fillna(method='bfill')
-        #batch = batch[:, :, 1:].swapaxes(2, 0).swapaxes(1, 2)  # (197, 11, 59)
+    result = dict()
+    for i, target_class in enumerate(targets):
+        result[target_class] = round(prediction[i])
 
-        # Data preprocessing - scaling the attributes
-        scalers = {}
-        for i in range(batch.shape[1]):
-            scalers[i] = preprocessing.MinMaxScaler(feature_range=(0, 1))
-            batch[:, i, :] = scalers[i].fit_transform(batch[:, i, :])
-        return batch  # tensor
-
-
-def load_model(target_name):
-    filename = 'models/model_' + target_name + '.h5'
-    if os.path.isfile(filename):
-        new_model = keras.models.load_model(filename)
-    else:
-        print((filename + ' not found'))
-    return new_model
+    return result
 
 
 if __name__ == '__main__':
