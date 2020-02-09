5c6c4bd3e192108f0ab8b0747ef0878787b646c3
dimstudio <dnldimtiri@gmail.com>
2020-02-09 02:39:45 +0100

    Added plots

--- feedback_descriptor.py
+++ feedback_descriptor.py
@@ -17,18 +17,33 @@ feedback = df[feedback_columns].drop_duplicates(keep='last').dropna()
 #print(feedback["Feedback.targetClass"])
 print("Shape of the feedback_data is: " + str(np.shape(feedback)))
 print("Shape of the annotation is: " + str(np.shape(annotations)))
-rates = annotations.set_index('start')[target_classes]
-#print(annotations['start'])
-errorRates = 1 - rates.rolling('10s').mean()
-ax = errorRates.plot(color=colors)
-handles, labels = plt.gca().get_legend_handles_labels() # get existing handles and labels
-ymin, ymax = ax.get_ylim()
-ax.vlines(x=feedback.index, ymin=ymin, ymax=ymax, color=colors, linestyles='dashed', label=feedback["Feedback.targetClass"])
-plt.ylabel('Error rate %')
-plt.xlabel('Compressions')
 
-handles.append(Line2D([0], [0], color='gray', linewidth=1, linestyle='--'))
-plt.legend(handles, labels+['Feedback'])
+rates_byid = annotations[target_classes+['RecordingID','start']].set_index('start')
+rates = annotations.set_index('start')[target_classes]
+
+
+errorRates = (1 - rates).rolling('10s').mean()
+derivative = (1 - rates).rolling('10s').apply(lambda x: x[-1] - x[0]) / 2
+fig = plt.figure()
+derivative.mean().plot(kind='bar')
+
+groups = rates_byid.groupby('RecordingID')
+
+for name,group in groups:
+
+    group = (1 - group[target_classes]).rolling('10s').mean()
+
+    ax=group.plot(color=colors)
+#ax = errorRates.plot(color=colors)
+    handles, labels = plt.gca().get_legend_handles_labels() # get existing handles and labels
+    ymin, ymax = ax.get_ylim()
+    ax.vlines(x=feedback.index, ymin=ymin, ymax=ymax, color=colors, linestyles='dashed', label=feedback["Feedback.targetClass"])
+    plt.ylabel('Error rate %')
+    plt.xlabel('Compressions')
+    plt.title('Error rate session: '+name)
+    handles.append(Line2D([0], [0], color='gray', linewidth=1, linestyle='--'))
+    plt.legend(handles, labels+['Feedback'])
+
 plt.show()
 #plt.savefig(session_folder+'/class-distribution.pdf')
 
