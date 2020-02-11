58128285a9199067036cca28fb42ea7af4d48d2e
dimstudio <dnldimtiri@gmail.com>
2020-02-11 10:33:29 +0100

    Last changes feedback descriptor

--- feedback_descriptor.py
+++ feedback_descriptor.py
@@ -10,18 +10,15 @@ session_folder = "manual_sessions/CPR_feedback_results"
 ignore_files = []
 target_classes = ['classRelease' , 'classDepth', 'classRate', 'armsLocked', 'bodyWeight']
 colors = ['tab:blue','tab:orange','tab:green','tab:red','tab:purple']
-colors_dict = {'classRelease':'b','classDepth':'g', 'classRate':'r', 'armsLocked':'c', 'bodyWeight':'m'}
+colors_dict = {'classRelease':u'tab:blue','classDepth':u'tab:orange', 'classRate':u'tab:green', 'armsLocked':u'tab:red', 'bodyWeight':u'tab:purple'}
 df, annotations = data_helper.get_feedback_from_files(session_folder, ignore_files=ignore_files)
 feedback_columns = [col for col in df.columns if 'Feedback' in col]
 feedback = df[feedback_columns].drop_duplicates(keep='last').dropna()
 #print(feedback["Feedback.targetClass"])
 print("Shape of the feedback_data is: " + str(np.shape(feedback)))
 print("Shape of the annotation is: " + str(np.shape(annotations)))
-
 rates_byid = annotations[target_classes+['RecordingID','start']].set_index('start')
 rates = annotations.set_index('start')[target_classes]
-
-
 errorRates = (1 - rates).rolling('10s').mean()
 derivative = (1 - rates).rolling('10s').apply(lambda x: x[-1] - x[0]) / 2
 fig = plt.figure()
@@ -32,12 +29,13 @@ groups = rates_byid.groupby('RecordingID')
 for name,group in groups:
 
     group = (1 - group[target_classes]).rolling('10s').mean()
-
+    sub_feedback = feedback[group.index[0]:group.index[-1]]
+    color_map = [colors_dict[t] for t in sub_feedback["Feedback.targetClass"].values]
     ax=group.plot(color=colors)
-#ax = errorRates.plot(color=colors)
+    #ax = errorRates.plot(color=colors)
     handles, labels = plt.gca().get_legend_handles_labels() # get existing handles and labels
     ymin, ymax = ax.get_ylim()
-    ax.vlines(x=feedback.index, ymin=ymin, ymax=ymax, color=colors, linestyles='dashed', label=feedback["Feedback.targetClass"])
+    ax.vlines(x=sub_feedback.index, ymin=ymin, ymax=ymax, color=color_map, linestyles='dashed', label=feedback["Feedback.targetClass"])
     plt.ylabel('Error rate %')
     plt.xlabel('Compressions')
     plt.title('Error rate session: '+name)
