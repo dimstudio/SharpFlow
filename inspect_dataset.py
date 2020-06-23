ff5a051ca0ca2fd193d5f1f10dd22419b51ce944
HansBambel <Kevin.Trebing@gmx.net>
2020-06-23 19:13:23 +0200

    Clearer view of dataset

--- inspect_dataset.py
+++ inspect_dataset.py
@@ -1,6 +1,13 @@
 from utils.dataset_transportation import transportation_dataset
 import numpy as np
 
+classes = {"Stationary": 0,
+           "Walking": 1,
+           "Running": 2,
+           "Car": 3,
+           "Train/Bus": 4,
+           "Biking": 5
+           }
 # Shows number of samples in dataset and classes
 data_folder = "manual_sessions/all_data/acc_magnitude"
 dataset = transportation_dataset(data_path=data_folder, train=True, use_magnitude=True)
@@ -9,6 +16,6 @@ test_dataset = transportation_dataset(data_path=data_folder, train=False, use_ma
 print("Whole dataset size:", len(dataset))
 existing_classes = np.unique(dataset.targets.numpy())
 print(existing_classes)
-for c in existing_classes:
-    print("Class: ", c, " amount: ", np.sum(dataset.targets.numpy() == c))
+for key, value in classes.items():
+    print(f"Class:  {key:15} amount: {np.sum(dataset.targets.numpy() == value)}")
 print(dataset.targets.bincount())
