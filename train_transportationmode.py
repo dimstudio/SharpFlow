c35dfe9af2fdf0e99976c1d1cb174262d7258a20
HansBambel <Kevin.Trebing@gmx.net>
2020-07-03 13:27:19 +0200

    Added evaluation on test set

--- train_transportationmode.py
+++ train_transportationmode.py
@@ -224,15 +224,78 @@ def train_model(data_folder, epochs, n_classes, batch_size, learning_rate, valid
                 'state_dict': load_best_val_model['state_dict']},
                f"models/trained_models/{model.__class__.__name__}.pt")
 
+    # Use best model for Test dataset:
+    conf_mat = ConfusionMatrix(num_classes=n_classes, normalized=False)
+    test_loss, class_report = eval_model(load_best_val_model, loss_func, test_dl, conf_mat, dev=dev)
+    # Use conf_mat to create metrics
+    conf_mat = conf_mat.value()
+    metrics_str = " ".join([f"{key} {class_report[key]['precision']:.2f}|{class_report[key]['recall']:.2f}|{class_report[key]['f1-score']:.2f}"
+                               for key in classes.keys()])
+    print(f"Test_loss: {test_loss:2.10f}, Test_acc: {class_report['accuracy']:.2f}"
+          f", Class-metrics (Precision|Recall|F1): {metrics_str}")
+
+
+def test_model(data_folder, model, batch_size, to_exclude=None, use_magnitude=False, valid_size=0.1, dev="cpu"):
+    sub_folder = "acc_magnitude" if use_magnitude else "all_sensors"
+    # If needed create dataset from session files in data_folder
+    if need_train_test_folder(os.path.join(data_folder, sub_folder)):
+        data_helper_transportation.create_train_test_folders(data_folder, sub_folder=sub_folder,
+                                                             to_exclude=to_exclude,
+                                                             acc_magnitude=use_magnitude, smoothing=True)
+    error_msg = "[!] valid_size should be in the range [0, 1]."
+    assert ((valid_size >= 0) and (valid_size <= 1)), error_msg
+
+    # load dataset
+    dataset = transportation_dataset(data_path=os.path.join(data_folder, sub_folder), train=True,
+                                     use_magnitude=use_magnitude)
+    test_dataset = transportation_dataset(data_path=os.path.join(data_folder, sub_folder), train=False,
+                                          use_magnitude=use_magnitude)
+    # Split the data into training and validation set
+    num_train = len(dataset)
+    split_valid = int(np.floor(valid_size * num_train))
+    split_train = num_train - split_valid
+    train_dataset, valid_dataset = random_split(dataset, [split_train, split_valid])
+    # Test dataset
+
+    # normalize dataset (using scaler trained on training set)
+    # get mean and std of trainset (for every feature)
+    mean_train = torch.mean(train_dataset.dataset.data[train_dataset.indices], dim=0)
+    std_train = torch.std(train_dataset.dataset.data[train_dataset.indices], dim=0)
+
+    test_dataset.data = (test_dataset.data - mean_train) / std_train
+    test_dl = DataLoader(test_dataset, batch_size, shuffle=False,
+                         num_workers=0, pin_memory=True)
+
+    loss_func = nn.CrossEntropyLoss().to(dev)
+    # Use best model for Test dataset:
+    conf_mat = ConfusionMatrix(num_classes=6, normalized=False)
+    test_loss, class_report = eval_model(model, loss_func, test_dl, conf_mat, dev=dev)
+    # Use conf_mat to create metrics
+    conf_mat = conf_mat.value()
+    metrics_str = " ".join([f"{key} {class_report[key]['precision']:.2f}|{class_report[key]['recall']:.2f}|{class_report[key]['f1-score']:.2f}"
+                               for key in classes.keys()])
+    print(f"Test_loss: {test_loss:2.10f}, Test_acc: {class_report['accuracy']:.2f}"
+          f", Class-metrics (Precision|Recall|F1): {metrics_str}")
+
 
 if __name__ == "__main__":
     dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-    train_model(data_folder="manual_sessions/all_data",
-                epochs=100,
-                batch_size=1024,
-                n_classes=6,
-                learning_rate=0.0001,
-                to_exclude=None,
-                use_magnitude=False,
-                earlystopping=30, dev=dev)
+    # train_model(data_folder="manual_sessions/all_data",
+    #             epochs=100,
+    #             batch_size=1024,
+    #             n_classes=6,
+    #             learning_rate=0.0001,
+    #             to_exclude=None,
+    #             use_magnitude=False,
+    #             earlystopping=30, dev=dev)
+
+    best_model = "models/trained_models/TransportationCNN.pt"
+    loaded_model = torch.load(best_model)
+    model = loaded_model["model"]
+    model.load_state_dict(loaded_model["state_dict"])
+    test_model(data_folder="manual_sessions/all_data",
+               model=model,
+               batch_size=1024,
+               use_magnitude=False,
+               dev=dev)
